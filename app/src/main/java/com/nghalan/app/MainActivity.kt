681abc42c1b4f6f3ccea07e7c2146fdf3ec396fb
HoracioJunior <juniorvilanculo95@gmail.com>
2022-04-25 09:34:03 -0700

    Primeira

---
+++ app/src/main/java/com/nghalan/app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nghalan.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash)
+    }
+}
